0549eff905da4a762cecc5573e951ae1cb011366
thiago.canalli@castbsb@ARANOT038 <thiago.canalli@castgroup.com.br>
2019-12-30 11:36:44 -0300

    Auto migration flyway

---
+++ src/main/resource/db/migration/V2_Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` VALUES 
+(2,'Araraquara,São Paulo, Brazil','Bernardo','Male','Sant\'Anna'),
+(3,'Araraquara,São Paulo, Brazil','Thiago','Male','Canalli'),
+(4,'Araraquara,São Paulo, Brazil','Alessandra','Male','Sant\'Anna');
